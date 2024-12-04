650e34e413d0c216017d74a9df49c7a7d0f7a0d7
yasuobear239 <vuphanhoanganh2309@gmail.com>
2024-12-04 09:20:04 +0700

    Create longest_increasing_subsequence.cpp

---
+++ longest_increasing_subsequence.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[10007], f[100007];
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=-1e8;
+    for (int i=1; i<=n; i++) cin>>a[i];
+    for (int i=1; i<=n; i++)
+    {
+        f[i]=1;
+        for (int j=1; j<=i; j++)
+        {
+            if(a[j]<a[i])
+            {
+                if(f[i]<f[j]+1)
+                {
+                    f[i]=f[j]+1;
+                }
+            }
+        }
+        ans=max(ans, f[i]);
+    }
+    cout<<ans;
+}
