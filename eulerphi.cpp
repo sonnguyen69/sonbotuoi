53c855ee4569ceb79f1a6a0c79b394aa1a4e2912
yasuobear239 <vuphanhoanganh2309@gmail.com>
2024-12-04 11:03:26 +0700

    Update eulerphi.cpp

--- eulerphi.cpp
+++ eulerphi.cpp
@@ -14,6 +14,8 @@ int eulerPhi(int n) {
 }
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int n;
     cin>>n;
     while(n--)
@@ -22,5 +24,4 @@ int main()
         cin>>t;
         cout<<eulerPhi(t)<<"\n";
     }
-
 }
