63786f2264337901d12b8fef8487730a0a53122a
yasuobear239 <vuphanhoanganh2309@gmail.com>
2024-12-04 08:12:31 +0700

    Create eulerphi.cpp

---
+++ eulerphi.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int eulerPhi(int n) {
+    if (n == 0) return 0;
+    int ans = n;
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) {
+            ans -= ans / i;
+            while (n % i == 0) n /= i;
+        }
+    }
+    if (n > 1) ans -= ans / n;
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int t;
+        cin>>t;
+        cout<<eulerPhi(t)<<"\n";
+    }
+
+}
