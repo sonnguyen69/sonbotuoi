96218b0041a6761f677c92691329024d39e5864b
youngQ1309 <besttulen07@gmail.com>
2024-12-05 21:15:20 +0700

    smith.cpp

---
+++ smith.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int tcs(long long n)
+{
+    int res=0;
+    while(n>0)
+    {
+        res+=n%10;
+        n/=10;
+    }
+    return res;
+}
+int main()
+{
+    long long n;
+    cin>>n;
+    int tam=tcs(n);
+    int tam2;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+            {
+                tam2+=tcs(i);
+                n=n/i;
+            }
+    }
+    if(n>1)
+        tam2+=tcs(n);
+    if(tam2==tam)
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
+
