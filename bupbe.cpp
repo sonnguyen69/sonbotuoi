2493e3e8a4c30aaad9ef661d978a944eac81a098
minhlol <51770829+minhlol@users.noreply.github.com>
2024-12-07 10:36:02 +0700

    small fixes

--- bupbe.cpp
+++ bupbe.cpp
@@ -2,36 +2,29 @@
 using namespace std;
 int a[100007];
 bool b[100007];
-int main()
-{
+int main() {
     int n, k;
-    cin>>n>>k;
-    for (int i=1; i<=n; i++)
-    {
-        cin>>a[i];
-        b[i]=1;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        b[i] = 1;
     }
-    sort (a+1, a+n+1, greater<int>());
-    long long tong=0;
-    int i=1, j=2;
-    while (i<=n&&j<=n)
-    {
-        if (a[i]-a[j]>=k)
-        {
-            if(b[i]==1) tong+=a[i];
-            b[i]=0;
-            b[j]=0;
+    sort(a + 1, a + n + 1, greater<int>());
+    long long tong = 0;
+    int i = 1, j = 2;
+    while (i <= n && j <= n) {
+        if (a[i] - a[j] >= k) {
+            if (b[i] == 1) tong += a[i];
+            b[i] = 0;
+            b[j] = 0;
             i++;
             j++;
-        }
-        else
-        {
+        } else {
             j++;
         }
     }
-    for(int i=1; i<=n; i++)
-    {
-        if(b[i]==1) tong+=a[i];
+    for (int i = 1; i <= n; i++) {
+        if (b[i] == 1) tong += a[i];
     }
-    cout<<tong;
+    cout << tong;
 }
--- noixich.cpp
+++ noixich.cpp
@@ -1,27 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[200001], i, n, tgian=0;
-int main()
-{
-   cin>>n;
-   for (i=1; i<=n; i++)
-        cin>>a[i];
-    sort (a+1, a+n+1);
-    i=1;
-   while (n>1)
-   {
-       a[i]--;
-       n--;
-       tgian++;
-
-           if (a[i]==0)
-           {n--;
+int a[200001], i, n, tgian = 0;
+int main() {
+    cin >> n;
+    for (i = 1; i <= n; i++) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    i = 1;
+    while (n > 1) {
+        a[i]--;
+        n--;
+        tgian++;
+        if (a[i] == 0) {
+            n--;
             i++;
-
-           }
-
-
-   }
-   cout<<tgian;
+        }
+    }
+    cout << tgian;
 }
 
