a7bbcb1bf678c481b2358dab4db947e01373c108
yasuobear239 <vuphanhoanganh2309@gmail.com>
2024-12-08 17:58:07 +0700

    Create hanh_trinh_bay.cpp

---
+++ hanh_trinh_bay.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, s1;
+    cin>>s>>s1;
+    int i=0, j=0;
+    while (i<s.size()&&j<s1.size())
+    {
+        if (s[i]==s1[j])
+        {
+            i++;
+            j++;
+        }
+        else i++;
+    }
+    if (j==s1.size()) cout<<"YES";
+    else cout<<"NO";
+}
