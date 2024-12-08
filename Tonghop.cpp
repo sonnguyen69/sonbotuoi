4b50107b6a1483e67877a1f600d37ce239e5ed1c
Minhhong2k7 <hongdaominh003@gmail.com>
2024-12-08 11:25:51 +0700

    Create Tonghop.cpp

---
+++ Tonghop.cpp
@@ -0,0 +1,31 @@
+//-------------Lí thuyết số-------------//
+//1.Ước và bội
+//2.Số hoàn hảo
+//3.UCLN,BCNN,Nguyên tố cùng nhau
+//4.Số fibonacci
+//5.Sàng nguyên tố và biến thể
+//6.Số bạn bè
+//7.Phi hàm euler
+//8.Cấp số cộng, cấp số nhân
+//9.Giải thuật euclid mở rộng
+//10.Nghịch đảo modulo
+//11.Lí thuyết đồng dư
+//12.Nhân ấn độ, luỹ thừa nhị phân
+//13.Hoán vị, tổ hợp,chỉnh hợp
+//14.Công thức legendre
+//15.Dãy catalan
+//16.Định lí fermat nhỏ
+//-------------Xử lí mảng-------------//
+//1.Xử lí xâu
+//2.Mảng cộng dồn, mảng hiệu
+//3.Mảng đánh dấu
+//4.Hai con trỏ
+//5.Cửa sổ trượt
+//6.Thuật toán sinh
+//7.Quay lui,vét cạn,nhánh cận,tham lam
+//8.Tìm kiếm nhị phân
+//-------------Lí thuyết đồ thị-------------//
+//1.Biểu diễn đồ thị
+//2.Thuật toán tìm kiếm
+//3.Các thuật toán trong đồ thị
+//-------------Quy hoạch động-------------//
