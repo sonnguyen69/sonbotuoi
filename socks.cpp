3267b8cab3fca951ebc51fa0be7df74737025f3c
yasuobear239 <vuphanhoanganh2309@gmail.com>
2024-12-08 16:44:02 +0700

    Create socks.cpp

---
+++ socks.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<int,int> mp;
+int a[100001], n, i;
+int main()
+{
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+        int x;
+        cin>>x;
+        mp[x]++;
+    }
+  int dem=0;
+  for (auto y:mp)
+    dem+=y.second/2;
+  cout<<dem;
+}
